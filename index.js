848b7c7d0f41d11d4b2204b79ce49f73defec03f
many3487 <manuelja@hotmail.es>
2023-08-30 12:24:44 -0500

    terminado

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const conectarDB = require('./config/db');
+const cors = require('cors');
 
 //Creo el servidor
 const app= express();
 
 //conectamos ala base de datos
 conectarDB();
-
+app.use(cors())
 app.listen(port =4000, () => {
     console.log('corriendo el servidor ','http://localhost:'+port)
 });
